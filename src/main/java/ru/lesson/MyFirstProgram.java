6ce1273d3d19763d5c2cdf98aa616593dd87cf1d
sbs-one <6616896@gmail.com>
2020-10-07 12:11:47 +0300

    Add few functions

---
+++ src/main/java/ru/lesson/MyFirstProgram.java
@@ -0,0 +1,29 @@
+package ru.lesson;
+
+public class MyFirstProgram {
+	public static void main(String[]args){
+	hello("world");
+	hello("user");
+	hello("Step");
+
+	double len = 5;
+		System.out.println("Площадь " + len + "=" +  area(len));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+}
+	public static void hello(String somebody) {
+
+		System.out.println("Hello " + somebody +  "!");
+	}
+
+	public static double area(double l) {
+		return  l * l;
+	}
+
+	public static double area(double a, double b) {
+		return  a*b;
+	}
+
+}
